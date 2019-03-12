747d72dfddf966d3429610a80ef25518a710aa3d
Golriz <golriz.gharavi@icloud.com>
2019-03-11 21:07:53 -0400

    Contact section completed!
    working on FAQ

---
+++ InspireWebApplication v1/InspireWebApplication/App/controllers/FAQController.js
@@ -0,0 +1,19 @@
+﻿
+
+app.controller('faqController', function (authorService) {
+
+    var vm = this;
+    vm.message = 'Golriz';
+    var promiseGet = authorService.getFAQs();
+    promiseGet.then(function (p1) {
+
+        vm.FAQs = p1.data.items;
+        alert(JSON.stringify(vm.FAQs))
+
+    },
+        function (errorP1) {
+            alert('FAILURE LOADING Employees', errorP1)
+        });
+
+
+})
